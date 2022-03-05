539505806196b164234d8169ac36ae9d67e979df
emylincon <emylincon@gmail.com>
2022-03-05 23:45:03 +0000

    tidy up

--- gotime.go
+++ gotime.go
@@ -6,19 +6,6 @@ import (
 
 // TODO
 // https://pkg.go.dev/time#RFC3339
-// 23/25/2014
-// 2014-25-23
-// 10 06 2014
-// 01 Jan 1970 00:00:00 GMT
-// 02 Jan 06 15:04 MST
-// 2011-10-10T14:48:00  2019-01-01T00:00:00
-// 2011-10-10T14:48:00.000+09:00
-// January 1, 1970 00:00:00 UTC
-// March 7, 2014
-// Aug 9, 1995
-// Wed, 09 Aug 1995 00:00:00 GMT
-// Wed, 09 Aug 1995 00:00:00
-// Mon Jan 2 15:04:05 MST 2006
 
 const (
 	// 2022/10/12, 2022-10-12, 2020 10 12
@@ -41,7 +28,6 @@ const (
 
 // Parse returns time.Time reprsentation of string
 func Parse(input string) (parsedTime time.Time, err error) {
-	// pattern := "[0-9]{2,4}(-|/)[0-9]{1,2}(-|/)[0-9]{1,2}"
 	switch {
 	case match(pattern5, input):
 		return formatPattern5(input)
